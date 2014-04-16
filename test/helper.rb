9bb7dccbb77f4d204688ed800eaa5ccdcc30ce28
Ashwin Hegde <ahegde@zendesk.com>
2014-04-16 16:09:08 -0700

    Readd require lib

--- test/helper.rb
+++ test/helper.rb
@@ -3,4 +3,8 @@ require 'test/unit'
 require 'mocha'
 require 'mocha/test_unit'
 require 'shoulda'
+
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+
 require 'changes_since'
